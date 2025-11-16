eb9995e25d334675218d7c920f7a87c1503bdd83
Malak-Yousry <es.mlk.yousry2023@alexu.edu.eg>
2025-11-16 19:47:24 +0200

    StudentLessonPanel

---
+++ src/StudentLessonPannel.java
@@ -0,0 +1,14 @@
+import javax.swing.JPanel;
+
+public class StudentLessonPannel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public StudentLessonPannel() {
+
+	}
+
+}
