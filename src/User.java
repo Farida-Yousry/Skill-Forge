276375e6ef48966366472c914718afdbb9335193
Farida-Yousry <es.farida.yousry2023@alexu.edu.eg>
2025-11-18 02:21:28 +0200

    solved conflicts

--- src/User.java
+++ src/User.java
@@ -5,24 +5,9 @@ import javax.swing.JOptionPane;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-<<<<<<< Updated upstream:src/UserAccount.java
-public class UserAccount extends Validations{
-
-
-	private String userName;
-	private String password;
-	private String email;
-	private String userId;
-	private String fullName;
-	private String role;
-	private int age;
-	
-	
-	public UserAccount(String username, String pass, String email, String id, String fullname, String role,int age){
-		userName = username;
-		password = pass;
-=======
 public class User{
 
 	protected String userName;
@@ -41,39 +26,10 @@ public class User{
 	public User(String userName, String password, String email, String userId, String role, int age) {
 		this.userName = userName;
 		this.password = password;
->>>>>>> Stashed changes:src/User.java
 		this.email = email;
 		this.userId = userId;
 		this.role = role;
 		this.age = age;
-<<<<<<< Updated upstream:src/UserAccount.java
-		
-	}
-	public UserAccount(){
-	}
-
-	public boolean login(String userName,String password,String role)  {
-		try(Scanner read = new Scanner(new File("Users.txt"))){
-			while(read.hasNextLine()) {
-				String data = read.nextLine().trim();
-				if(data.isEmpty())continue;
-				String[] hold = data.split(",");
-				
-				String name = hold[0];
-				String pass = hold[1];
-				String r = hold[2];
-				
-				if(name.equals(userName) && pass.equals(hashPassword(password)) && r.equals(role))
-					return true;
-			}
-		}
-		catch(FileNotFoundException e) {
-			System.out.println("Error");
-			e.printStackTrace();
-		}
-		return false;
-	}
-=======
 	}
 	
 	public String getUserName() {
@@ -141,7 +97,6 @@ public class User{
 	
 }
 
->>>>>>> Stashed changes:src/User.java
 	 public void logout() {
 		 System.exit(0);
 	 }
@@ -156,48 +111,6 @@ public class User{
 		 JOptionPane.showMessageDialog(null,"ID --> " + id);
 		 return id;
 		
-<<<<<<< Updated upstream:src/UserAccount.java
-	}
-	public void signup(String fullName,String userName,String password,int age,String role,String email) {
-		if(!validateFullName(fullName))
-		    JOptionPane.showMessageDialog(null,"Invalid name format[Please Enter at least 3 names]");
-		else if(!validateEmail(email))
-			 JOptionPane.showMessageDialog(null,"Invalid Email format");
-		else if(!validateUserName(userName))
-			JOptionPane.showMessageDialog(null,"Invalid Username format[Please Enter at least 5 characters]");
-		else if(!validatePassword(password))
-			JOptionPane.showMessageDialog(null,"Invalid Password format[Please Enter at least 3 digits ]");
-		else if(!validateAge(age))
-			JOptionPane.showMessageDialog(null,"Invalid age");
-	    else {
-		this.userName = userName.trim();
-		this.password = hashPassword(password);
-		this.email = email.trim();
-		this.userId = generateNewId(role);
-		this.fullName = fullName.trim();
-		this.role = role;
-		this.age = age;
-		//	saveToFile();
-		//JOptionPane.showMessageDialog(null,"Account Created");
-	}
-	}
-	public String getUserId(){
-		return userId;
-	}
-	public String getUserName(){
-		return userName;
-	}
-	public String getPass(){
-		return password;
-	}
-	public String getEmail(){
-		return email;
-	}
-	public String getRole(){
-		return role;
-	}
-=======
 	}*/
 
->>>>>>> Stashed changes:src/User.java
 }
